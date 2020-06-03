1967fa346032ad0769f9aa42f51316a3e4646444
Teodoro Delson Jr <youremail@gmail.com>
2020-06-03 16:20:33 -0700

    final commit

--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,13 @@ import { TranscriptRouter } from "../src/Routes/Transcript";
 import cors from "cors";
 
 export const app = express();
+// app.use(
+//   cors({
+//     credentials: true,
+//     origin: "http://localhost:3000",
+//   })
+// );
+
 app.use(cors());
 app.use(bodyParser.json());
 app.use(sessionMiddleware);
